2a59b1186734f67f219a60fcc5cedf7ce6c2b832
Yaren Duman <yarenduman200705@gmail.com>
2026-05-19 15:24:06 +0300

    updated brick dimensions to fit on the screen

--- include/definitions.hpp
+++ include/definitions.hpp
@@ -14,8 +14,8 @@ const float BALL_SPEED_Y = -4.0f; // pixels per second
 const float PADDLE_WIDTH = 100.0f;
 const float PADDLE_HEIGHT = 20.0f;
 const float BALL_RADIUS = 10.0f;
-const float BRICK_WIDTH = 80.0f;
-const float BRICK_HEIGHT = 25.0f;
+const float BRICK_WIDTH = 50.0f;
+const float BRICK_HEIGHT = 10.0f;
 const float BRICK_OFFSET = 10.0f; // space between bricks
 
 struct GameBrick {
